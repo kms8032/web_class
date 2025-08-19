83939acc4274b5b7857efe5709a8dfdaa26122e3
kms8032 <alstjr803232@g.yju.ac.kr>
2025-08-19 20:16:56 +0900

    fix : DB 스키마 옵션 추가

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS users (
     no INT AUTO_INCREMENT PRIMARY KEY, -- 순번 (자동 증가)
     id VARCHAR(20) NOT NULL UNIQUE, -- 학번 (유일)
     password VARCHAR(100) NOT NULL, -- 해시된 비밀번호 (해싱 필요)
-    name VARCHAR(50) NOT NULL, -- 이름
-);
+    name VARCHAR(50) NOT NULL -- 이름
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 -- posts 테이블 생성
 CREATE TABLE IF NOT EXISTS posts (
@@ -18,5 +18,5 @@ CREATE TABLE IF NOT EXISTS posts (
     writer VARCHAR(50) NOT NULL, -- 작성자
     post_pw VARCHAR(100) NOT NULL, -- 게시글 비밀번호 (수정, 삭제)
     title VARCHAR(255) NOT NULL, -- 게시글 제목
-    content VARCHAR(255) NOT NULL -- 게시글 내용
-)
+    content TEXT NOT NULL -- 게시글 내용
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
